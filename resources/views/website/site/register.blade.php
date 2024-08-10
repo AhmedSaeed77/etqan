a73d2f6638134e7642b3aa6e260f3f72ec134e33
Essam Tayeh Ahmed <58932096+Essam1421996@users.noreply.github.com>
2024-08-10 15:20:37 +0300

    Update register.blade.php

--- resources/views/website/site/register.blade.php
+++ resources/views/website/site/register.blade.php
@@ -7,7 +7,7 @@
       <div class="contact_us gap-5 py-5 d-grid gap-5">
         <div >
           <h2 class="page_title">  التسجيل</h2>
-          <p>التسجيل على الموقع لكى تستفيد بكل جديد</p>
+          <p>التسجيل في الموقع لكى تستفيد بكل جديد</p>
         </div>
         <div class="d-grid d-md-flex justify-content-center gap-5 align-items-start ">
           <div class="contact_form  order-1 order-md-0 ">
